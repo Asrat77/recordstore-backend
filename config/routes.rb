2e4d3132224b9fe6935cf7fc4e57d73ecb64acbd
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-06-06 17:23:45 +0300

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  
+  namespace :api do
+    namespace :v1 do
   resources :records
   resources :artists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    end
+  end
+  root to: 'home#index'
+
+  
 end
